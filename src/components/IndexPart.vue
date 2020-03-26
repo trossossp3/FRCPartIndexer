66483ca2d5c0c6f088c4c8c49afdac8a0052717d
Max <50331421+Apitz-Grossman@users.noreply.github.com>
2020-03-26 10:35:34 -0400

    minor change i forgot to push earlier

--- src/components/IndexPart.vue
+++ src/components/IndexPart.vue
@@ -6,7 +6,7 @@
         <div class="row">
           <div class="col-md-10"></div>
           <div class="col-md-2">
-            <router-link :to="{ name: 'create' }" class="btn btn-primary">Create Post</router-link>
+            <router-link :to="{ name: 'create' }" class="btn btn-primary">Create Part</router-link>
           </div>
         </div><br />
 
