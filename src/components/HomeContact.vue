cb149ffeb334135c5b2d9a66c410bab618a008bc
Max <50331421+Apitz-Grossman@users.noreply.github.com>
2020-04-08 19:18:01 -0400

    position

--- src/components/HomeContact.vue
+++ src/components/HomeContact.vue
@@ -3,8 +3,8 @@
 
   <div>
     <h1 style="font-size:80px;  font-family: Andale Mono, monospace; color: white; position: absolute; left:8%;">FRC TEAM 7558 PART INDEXER</h1>
-    <h1 style="font-size:20px; text-align:center; color: white; position: absolute; top: 22.5%;">Welcome to Convenience. Welcome to Security. Welcome Home.</h1>	
-    <h1 style="font-size:20px; text-align:center; color: white; position: absolute; top:27.5%;">In order to begin, please select "Create Part" on the taskbar.</h1>	
+    <h1 style="font-size:20px; text-align:center; color: white; position: absolute; top: 22.5%; left: 12%;">Welcome to Convenience. Welcome to Security. Welcome Home.</h1>	
+    <h1 style="font-size:20px; text-align:center; color: white; position: absolute; top:27.5%; left: 15.4%;">In order to begin, please select "Create Part" on the taskbar.</h1>	
     <img src="@/assets/robo_cad.png" style="position: relative; left: 50%; transform: translate(-40%, -25%);"/>
 
   </div>
